e5ff2dd54960197d6cc1f89cc3cdf206baba46ed
Mahesh Sankhala <maheshsainni@gmail.com>
2025-08-24 14:21:04 +0530

    Pieces of Eight font added and homepage banner updated

--- src/components/complexFooter.tsx
+++ src/components/complexFooter.tsx
@@ -19,8 +19,8 @@ export default function ComplexFooter() {
               <script>
                 document.write(new Date().getFullYear())
               </script>
-              &nbsp;Astro Ecommerce by &nbsp;
-              <a href="https://www.creative-tim.com" className="text-dark" target="_blank">Creative Tim</a>.
+              &nbsp;Urban Panchhi &nbsp;
+              {/* <a href="https://www.creative-tim.com" className="text-dark" target="_blank">Creative Tim</a>. */}
             </div>
           </div>
         </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,10 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-sticky py-3 start-0 end-0">
       <div className="container px-1">
-        <a className="navbar-brand font-weight-bolder ms-lg-0 " href="/">Urban Panchhi</a>
+        <a className="navbar-brand font-weight-bolder ms-lg-0 " href="/">
+          <img src={"../images/logo.png"} className="avatar  me-3 " alt="user image" />
+          <span className="site-logo-title">Urban Panchhi</span>
+        </a>
         <button className="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon mt-2">
             <span className="navbar-toggler-bar bar1"></span>

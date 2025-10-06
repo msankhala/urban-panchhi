aba907cc12457f5c3f07b7de468eb74857241689
Mahesh Sankhala <maheshsainni@gmail.com>
2025-10-06 23:27:47 +0530

    React spa support added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import react from "@astrojs/react";
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   site: 'https://urbanpanchhi.com',
-  base: '/'
+  base: '/',
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });

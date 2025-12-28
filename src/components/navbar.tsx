5b0071f7cef839fb140f330b80a17112bb200b66
Mahesh Sankhala <maheshsainni@gmail.com>
2025-12-28 20:06:13 +0530

    Lot 1 product added

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -26,8 +26,8 @@ const Navbar = () => {
               </a>
             </li> */}
             <li className="nav-item">
-              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="/dhurrie/">
-                Dhurrie
+              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="/durrie/">
+                Durrie
               </a>
             </li>
             {/* <li className="nav-item dropdown">
